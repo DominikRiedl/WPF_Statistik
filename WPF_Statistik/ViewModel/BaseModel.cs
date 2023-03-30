60f1756956b611ca291d0d33951387ab5c2417fe
riedld2 <riedld2@NUCC00910>
2023-03-30 14:29:21 +0200

    BaseModel.cs eingebunden

--- WPF_Statistik/ViewModel/BaseModel.cs
+++ WPF_Statistik/ViewModel/BaseModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPF_Statistik.ViewModel
 {
-    internal class BaseModel
+    public class BaseModel : INotifyPropertyChanged
     {
+        // Declare the PropertyChanged event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // OnPropertyChanged will raise the PropertyChanged event passing the
+        // source property that is being updated.
+        protected void OnPropertyChanged(String propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
