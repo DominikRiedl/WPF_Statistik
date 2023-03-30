dd80bd43d8d457e036dd5cddc20859fc2358144d
riedld2 <riedld2@NUCC00910>
2023-03-30 14:14:36 +0200

    init2

---
+++ WPF_Statistik/Persistenz/Data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WPF_Statistik
+{
+  public class Data
+  {
+    public String Bezeichnung { get; set; }
+
+    public double Wert { get; set; }
+
+    public Brush Farbe { get; set; }
+
+    public Data()
+    {
+      Random r = new Random();
+      this.Farbe = new SolidColorBrush(Color.FromRgb((byte)r.Next(0,256), (byte)r.Next(0, 256), (byte)r.Next(0, 256)));
+    }
+
+    public Data(String bez, double wert, Brush farbe)
+    {
+      this.Bezeichnung = bez;
+      this.Wert = wert;
+      this.Farbe = farbe;    
+    }
+
+    public static List<Data> AlleLesen()
+    {
+      // hier wird in der Realität aus der Datenbank gelesen!
+      List<Data> lst = new List<Data>();
+      lst.Add(new Data("Tony", 47.2, Brushes.Red));
+      lst.Add(new Data("Lisa", 7.9, Brushes.Blue));
+      lst.Add(new Data("Susi", 17.2, Brushes.Yellow));
+      lst.Add(new Data("Fred", 67.8, Brushes.Orange));
+
+      return lst;
+    }
+  }
+}
