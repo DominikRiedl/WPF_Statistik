b3ccd897043b797fac1b1afdc4b10a1a046c5a57
riedld2 <riedld2@NUCC00910>
2023-03-31 09:28:30 +0200

    Canvas mit Balkendiagramm implementiert

--- WPF_Statistik/View/MainWindow.xaml.cs
+++ WPF_Statistik/View/MainWindow.xaml.cs
@@ -20,9 +20,74 @@ namespace WPF_Statistik
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Data> lstData = new List<Data>();
+        int i = 0;
+        double ObjWidth = 0;
+        double ObjHeight = 0;
+        int counter = 0;
+        double posX = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LbData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbData.SelectedItem == null)
+            {
+                btDel.IsEnabled = false;
+            }
+            else
+            {
+                btDel.IsEnabled = true;
+            }
+        }
+
+        private void btNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FillCanvas()
+        {
+            lstData = Data.AlleLesen();
+
+            i = lstData.Count;
+            ObjWidth = cnvStats.ActualWidth / i;
+            ObjHeight = cnvStats.ActualHeight;
+
+
+            foreach (var item in lstData)
+            {
+                Label lb = new Label();
+
+                lb.Content = item.Bezeichnung;
+                lb.Background = item.Farbe;
+                lb.Height = item.Wert;
+                lb.Width = ObjWidth;
+
+                posX = ObjWidth * counter;
+
+                Canvas.SetBottom(lb, 0);
+                Canvas.SetLeft(lb, posX);
+
+                cnvStats.Children.Add(lb);
+
+                counter++;
+            }
+
+            counter = 0;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillCanvas();
         }
     }
 }
